24318c4afca4e15c04b5d24a4f6606ac30d89345
giagiar <giagiar.x@gmail.com>
2019-05-29 17:35:28 +0800

    autoplay

--- script/jsfunction.js
+++ script/jsfunction.js
@@ -1,12 +1,14 @@
-var slideIndex = 0;
+var slideIndex = 1;
+var timer;
 showSlides(slideIndex);
 
 function plusSlides(n) {
-  showSlides(slideIndex += n);
+  showSlides(slideIndex += n-1);
 }
 
 function currentSlide(n) {
   showSlides(slideIndex = n);
+
 }
 
 function showSlides(n) {
@@ -18,10 +20,18 @@ function showSlides(n) {
   for (i = 0; i < slides.length; i++) {
       slides[i].style.display = "none";  
   }
+
   for (i = 0; i < dots.length; i++) {
       dots[i].className = dots[i].className.replace(" active", "");
   }
-  slides[slideIndex-1].style.display = "block";  
-    setTimeout(showSlides, 2000);
+  if (slideIndex > slides.length) {slideIndex = 1}
+  slides[slideIndex-1].style.display = "block";      
   dots[slideIndex-1].className += " active";
+  autoplay();
+}
+
+function autoplay(){
+    clearTimeout(timer);
+    timer=setTimeout(showSlides, 6000);
+    slideIndex++;
 }
